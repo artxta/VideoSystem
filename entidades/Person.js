1ce2ccab2a974289dc02dd29bcb26283426f533a
Jose Alberto Torresano <joseichiban91@gmail.com>
2026-01-22 19:36:44 +0100

    Creación de Person

--- entidades/Person.js
+++ entidades/Person.js
@@ -1,5 +1,11 @@
 "use strict";
 
+import {
+  BaseException,
+  InvalidAccessConstructorException,
+  EmptyValueException,
+} from "../exceptions/exceptions.js";
+
 // Objeto para identificar los datos de una persona
 class Person {
   // campos privados
@@ -13,10 +19,83 @@ class Person {
     // comprobar si se llama con new
     if (!new.target) throw new InvalidAccessConstructorException();
 
+    // validar parametros
+    if (!name) throw new EmptyValueException("name");
+    if (!lastname1) throw new EmptyValueException("lastname1");
+    // si born es una fecha o una string
+    const date = born instanceof Date ? born : new Date(born);
+    if (isNaN(date.getTime())) throw new EmptyValueException("born");
+
+    // definir los atributos
     this.#name = name;
     this.#lastname1 = lastname1;
     this.#lastname2 = lastname2;
-    this.#born = new Date(born);
+    this.#born = date;
     this.#picture = picture;
+
+    // name
+    Object.defineProperty(this, 'name', {
+      enumerable: true,
+      get() {
+        return this.#name;
+      },
+      set(value) {
+        if (!value) throw new EmptyValueException("name");
+        this.#name = value;
+      }
+    });
+
+    // lastname1
+    Object.defineProperty(this, 'lastname1', {
+      enumerable: true,
+      get() {
+        return this.#lastname1;
+      },
+      set(valor) {
+        if (!valor) throw new EmptyValueException("lastname1");
+        this.#lastname1 = valor;
+      }
+    });
+
+    // lastname2
+    Object.defineProperty(this, 'lastname2', {
+      enumerable: true,
+      get() {
+        return this.#lastname2;
+      },
+      set(valor) {
+        if (!valor) throw new EmptyValueException("lastname2");
+        this.#lastname2 = valor;
+      }
+    });
+
+    // born
+    Object.defineProperty(this, 'born', {
+      enumerable: true,
+      get() {
+        return this.#born;
+      },
+      set(value) {
+        const date = value instanceof Date ? value : new Date(value);
+        if (isNaN(date.getTime())) throw new EmptyValueException("born");
+        this.#born = date;
+      }
+    });
+
+    // picture
+    Object.defineProperty(this, 'picture', {
+      enumerable: true,
+      get() {
+        return this.#picture;
+      },
+      set(value) {
+        if (!value) throw new EmptyValueException('picture');
+        this.#picture = value;
+      }
+    });
+
+
   }
 }
+
+export { Person };
